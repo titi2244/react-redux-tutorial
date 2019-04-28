f81a38ca4a1149df4784b821218e43174e648f56
yspark <yspark@nextree.co.kr>
2019-04-28 19:17:53 +0900

    apply redux-dev-tool

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ import reducers from './reducers';
 import { Provider } from 'react-redux';
 
 // 스토어 생성
-const store = createStore(reducers);
+// const store = createStore(reducers);
 
+// redux-dev-tool사용을 위한 스토어 생성
+
+const store = createStore(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
     <Provider store={store}>
         <App />
